0a1d9900fe9565e5e03b2e7bf99ed2f97894f19a
Kenny Weiss <kenny1weiss@aol.com>
2023-03-24 13:42:36 -0700

    Check answerbox, not fully functional

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 let num1 = document.querySelector("#num1");
 let num2 = document.querySelector("#num2");
 
-var num1Value = Math.floor(Math.random() * 10);
-var num2Value = Math.floor(Math.random() * 10);
+let num1Value;
+let num2Value;
+let sum;
 
 function getNewValue(){
     return Math.floor(Math.random() * 10);
 }
 
-function newProblem(){
+function newProblem(num1Value, num2Value){
     num1Value = getNewValue();
     num2Value = getNewValue();
+    sum = num1Value + num2Value;
     num1.innerText = num1Value;
     num2.innerText = num2Value;
+}
+
+let userAnswer = document.querySelector("#answerBox");
+function checkAnswer(){
+    if(userAnswer == sum){
+        alert("`user: ${userAnswer}` ");
+    }
+    alert(sum);
 }
